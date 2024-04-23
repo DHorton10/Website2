d8e9dfe8d311b6d001cfe564138708c3b8b8a00d
DHorton10 <143060210+DHorton10@users.noreply.github.com>
2024-04-23 21:50:40 +0000

    Tue, Apr 23, 2024, 2:50 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -41,7 +41,15 @@ function updateWrongLettersEl() {
     ${wrongLetters.map(letter => `<span>${letter}</span>`)}
   `
 
-  figureParts.forEach()
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+
+    if (index < errors) {
+      part.style.display = 'block'
+    } else {
+      part.style.display = 'none'
+    }
+  })
 }
 
 // Show Notification
